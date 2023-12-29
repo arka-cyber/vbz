352344e6700a185323ff58e73fbe8064d403818d
arka-cyber <153489163+arka-cyber@users.noreply.github.com>
2023-12-29 10:50:31 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const contactButton = document.getElementById('contactButton');
-    const contactForm = document.getElementById('contactForm');
+    const ratingContainers = document.querySelectorAll('.rating');
 
-    contactButton.addEventListener('click', function () {
-        contactForm.style.display = 'block';
-        contactButton.style.display = 'none';
+    ratingContainers.forEach(container => {
+        container.addEventListener('click', function (e) {
+            const stars = container.querySelectorAll('.star');
+            const clickedStar = e.target;
+
+            // Reset all stars to empty
+            stars.forEach(star => star.classList.remove('filled'));
+
+            // Fill the clicked star and all previous stars
+            stars.forEach(star => {
+                star.classList.add('filled');
+                if (star === clickedStar) {
+                    return false; // Stop when the clicked star is reached
+                }
+            });
+        });
     });
 });
